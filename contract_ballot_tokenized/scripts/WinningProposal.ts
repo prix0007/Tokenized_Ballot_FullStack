60b9f088255692f2ab778e0748c06b4f0f71e9b4
Prince Anuragi <princanurag07@gmail.com>
2022-10-15 03:43:44 +0530

    Initial Commit

---
+++ contract_ballot_tokenized/scripts/WinningProposal.ts
@@ -0,0 +1,51 @@
+import { ethers } from "hardhat";
+import {
+  TokenizedBallot__factory,
+} from "../typechain-types";
+
+async function main() {
+  const proposal_to_give_vote = 0;
+
+  const key = process.env.PRIVATE_KEY;
+  console.log({ key: `0x${key}` });
+  if (!key) {
+    console.log("NO PRIVATE KEY FOUND!!");
+    return;
+  }
+
+  const TOKENIZED_BALLOT = process.env.TOKENIZED_BALLOT;
+
+  if (!TOKENIZED_BALLOT) {
+    console.error("TOKENIZED_BALLOT not set in env");
+    return;
+  }
+
+  console.log(TOKENIZED_BALLOT);
+
+  const wallet = new ethers.Wallet(ethers.utils.hexlify(`0x${key}`));
+  const signer = wallet.connect(ethers.provider);
+
+  const tokenizedBallotFactory = new TokenizedBallot__factory(signer);
+  const tokenizedBallotContract =
+    tokenizedBallotFactory.attach(TOKENIZED_BALLOT);
+
+  const proposal = await tokenizedBallotContract.winningProposal();
+  console.log(proposal);
+  const proposalName = await tokenizedBallotContract.winnerName();
+  console.log(proposalName);
+
+  console.log(
+    `The winning proposal ${proposal}`
+  );
+
+  console.log(
+    `The winning proposal name ${ethers.utils.parseBytes32String(proposalName)}`
+  );
+}
+
+main()
+  .then(() => process.exit(0))
+  .catch((error) => {
+    console.error(error);
+    process.exit(1);
+  });
