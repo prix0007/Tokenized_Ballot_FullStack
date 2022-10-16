b61160cee76347274ec18c6821136a1408eeecad
Arash Fatahzade <fatahzade@gmail.com>
2022-10-16 11:35:03 +0330

    Enabled cors

--- backend_tokenized_ballot/src/main.ts
+++ backend_tokenized_ballot/src/main.ts
@@ -4,6 +4,13 @@ import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  const corsOptions = {
+    origin: '*',
+    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
+    preflightContinue: false,
+    optionsSuccessStatus: 204,
+  };
+  app.enableCors(corsOptions);
 
   const config = new DocumentBuilder()
     .setTitle('Tokenized Ballot Backend')
